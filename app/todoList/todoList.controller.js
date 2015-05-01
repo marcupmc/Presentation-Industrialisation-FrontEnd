e7989abf0b82d9a53b38722b2f20c39b7316382e
marcgregoire <marc.gregoire1989@gmail.com>
2015-05-01 17:26:20 +0200

    Gestion des taches terminees

--- app/todoList/todoList.controller.js
+++ app/todoList/todoList.controller.js
@@ -3,14 +3,28 @@ TodoList.controller('TodoListCtrl',['$scope',function($scope){
    $scope.currentTask="";
    $scope.listOfTasks=[];
 
+
    $scope.addTask=function(){
-        var newTask={
+        var newTask=
+        {
             text:$scope.currentTask,
             done:false
-        }
+        };
         $scope.listOfTasks.unshift(newTask);
         $scope.currentTask="";
-   }
+   };
+
+   $scope.doTask= function(index){
+        var doneTask  = $scope.listOfTasks[index];
+        //on supprime la tache de la liste
+        $scope.listOfTasks.splice(index, 1);
+        //et on l'ajoute à la fin si c'est une tache finie
+        if(doneTask.done)
+            $scope.listOfTasks.push(doneTask);
+        else
+         //Sinon on la remet au debut
+            $scope.listOfTasks.unshift(doneTask);
+   };
 
 
 }]);
